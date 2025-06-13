020fcdd9c4691407e7d2375bc8a8424876507e16
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-06-13 23:44:24 +0700

    noe

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 flex fixed w-full left-0 text-white z-50 justify-center bottom-0 shadow-lg text-sm items-center px-4 py-5">
+        <footer className="bg-gray-800 text-white text-sm px-4 py-3 flex justify-center items-center">
             <p className="text-center w-full">
-                &copy; {new Date().getFullYear()} HizkiaDev. All right reserved
+                &copy; {new Date().getFullYear()} HizkiaDev. All rights reserved.
             </p>
         </footer>
-    )
+    );
 };
 
 export default Footer;
